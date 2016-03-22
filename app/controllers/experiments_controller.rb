aa27911979e6101475699efea5e933b40ac7ff38
Gonzalo <gonzalopatricio88@gmail.com>
2016-03-22 20:56:48 -0300

    New navbar in bootstrap layout

--- app/controllers/experiments_controller.rb
+++ app/controllers/experiments_controller.rb
@@ -1,10 +1,16 @@
 class ExperimentsController < ApplicationController
+
+  #layout: "bootstrap"
+
   def page1
+    render layout: "bootstrap"
   end
 
   def page2
+    render layout: "bootstrap"
   end
 
   def page3
+    render layout: "bootstrap"  
   end
 end
