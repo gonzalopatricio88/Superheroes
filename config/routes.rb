4e108b9b0a96a5196785f7450d1676d4e698a62c
Gonzalo <gonzalopatricio88@gmail.com>
2016-03-22 20:21:51 -0300

    New controller: experiments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
+  get 'experiments/page1'
+
+  get 'experiments/page2'
+
+  get 'experiments/page3'
+
   get 'pages/batman'
 
   get 'pages/superman'
+
+  get 'pages/batman_vs_superman'
+
+  root 'pages#batman_vs_superman'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
